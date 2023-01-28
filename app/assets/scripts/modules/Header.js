1ea98346bce12ba9ef24b5840c1ee76277286f7d
Brian Thomas <btdeveloper125@gmail.com>
2023-01-28 11:09:43 -0700

    refactor(Fonts): Updated Font access
    Added new font for bold and black
    Restructured list items to apply specific bold to list number

--- app/assets/scripts/modules/Header.js
+++ app/assets/scripts/modules/Header.js
@@ -11,14 +11,26 @@ const Header = ({ tab }) => {
           </section>
           <section className='header__section-right'>
             <svg version="1.1" className="header__divider-line" x="0px" y="0px"  >
-              <path className="path2" fill="#979797" strokeWidth="3" stroke="#979797" d="M0 0 l1120 0" />
+              <path className="path2" strokeWidth="2.3" d="M0 0 l1120 0" />
             </svg>
             <nav className='header__nav-outer'>
               <ul className='header__nav-inner'>
-                <li className='header__nav-item'>00 Home</li>
-                <li className='header__nav-item'>01 Destination</li>
-                <li className='header__nav-item'>02 Crew</li>
-                <li className='header__nav-item'>03 Technology</li>
+                <li className='header__nav-item'>
+                  <span className='header__nav-item-order'>00</span> 
+                  <p className='header__nav-item-text'>Home</p>
+                </li>
+                <li className='header__nav-item'>
+                  <span className='header__nav-item-order'>01</span> 
+                  <p className='header__nav-item-text'>Destination</p>
+                </li>
+                <li className='header__nav-item'>
+                  <span className='header__nav-item-order'>02</span> 
+                  <p className='header__nav-item-text'>Crew</p>
+                  </li>
+                <li className='header__nav-item'>
+                  <span className='header__nav-item-order'>03</span> 
+                  <p className='header__nav-item-text'>Technology</p>
+                </li>
               </ul>
             </nav>
           </section>
