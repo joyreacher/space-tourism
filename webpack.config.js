368beaf10bfa5d88411eea9707732e56c99bbfaf
Brian Thomas <btdeveloper125@gmail.com>
2023-01-28 09:07:33 -0700

    refactor(React): Added react support
    Also added npm build command/logic
    webpack config file contains build and dev logic
    build outputs a docs file within the project root

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,87 @@
 const path = require('path')
+const fse = require('fs-extra');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const postCSSPlugins = 
+    // list of plugins we need
+    require('postcss-import');
+    require('postcss-simple-vars');
+    require('postcss-mixins');
+    require('postcss-nested');
+    require('autoprefixer');
 
-const postCSSPlugins = [
-  require('postcss-import'),
-  require('postcss-mixins'),
-  require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('autoprefixer')
-]
-
-module.exports = {
-  entry: './app/assets/scripts/App.js',
-  output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app')
-  },
-  devServer: {
-    static: path.join(__dirname, 'app'),
-    hot: true,
-    port: 3000,
-    host: '0.0.0.0'
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader", 
-            { 
-            loader: "postcss-loader", 
-            options: {
-              postcssOptions: 
-              {
-                plugins: postCSSPlugins
-                }
-              }
-            }
-        ],
-        
-      }
-    ]
+let currentTask = 'dev'
+class RunAfterCompile{
+  apply(compiler){
+    compiler.hooks.done.tap('copy imgs', function(){
+      fse.copySync('./app/assets/images', './docs/assets/images')
+    })
   }
 }
+let pages = fse.readdirSync('./app').filter((file)=>{
+  return file.endsWith('.html');
+}).map((page)=>{
+  return new HtmlWebpackPlugin({
+    filename: page,
+    template: `./app/${page}`,
+    minify: false,
+  })
+})
+let cssConfig = {
+  test: /\.css$/i,
+  use: ['css-loader', {loader: 'postcss-loader', options: {postcssOptions: {plugins: [postCSSPlugins, require.resolve('postcss-simple-vars'),
+  require.resolve('postcss-nested'), require.resolve('autoprefixer'), postCSSPlugins({ myOption: true })]}}}]
+}
+let config = {
+  entry: './app/assets/scripts/App.js' ,
+  plugins: pages,
+  module: {
+    rules:[
+        //?how to tell webpack to do something different 
+        cssConfig,
+        {
+          test:/\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react','@babel/preset-env']
+            }
+          }
+        }
+        ]
+      }
+}
+if(currentTask === 'build'){
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js', 
+    path: path.resolve(__dirname, 'docs')
+}
+  config.mode = "production"
+  config.optimization = {
+    splitChunks: {chunks:'all'}
+  }
+  config.plugins.push(
+    new CleanWebpackPlugin(), 
+    new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    new RunAfterCompile()
+    )
+}
+if(currentTask === 'dev'){
+  cssConfig.use.unshift("style-loader")
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  }
+  config.devServer = {
+    static: path.join(__dirname, 'app'),
+    hot: true, 
+    port: 3000,
+    host: '0.0.0.0',
+  }
+  config.mode = "development"
+}
+module.exports = config
