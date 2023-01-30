399711d6bcaaf81304b1fe47ad9db72d03256650
Brian Thomas <btdeveloper125@gmail.com>
2023-01-30 13:08:35 -0700

    refactor(React): Update to work with React 17

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,17 @@
 import '../styles/styles.css'
 import 'lazysizes'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import Header from './modules/Header';
 import Footer from './modules/Footer';
 
 
-const footer = createRoot(document.querySelector('.footer'))
-const navbar = createRoot(document.querySelector('.header'));
 // Check for page title
 let title = () => {
   return document.title
 }
-navbar.render(<Header active={title} />);
-footer.render(<Footer />);
-
+ReactDOM.render(<Footer/>, document.querySelector('.footer'))
+ReactDOM.render(<Header active={title}/>, document.querySelector('.header'))
 
 if (module.hot) {
   module.hot.accept()
