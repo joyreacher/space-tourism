f7447a3619eb4c9d76777602a013f7105be59e77
Brian Thomas <btdeveloper125@gmail.com>
2023-02-03 10:32:14 -0700

    refactor(webpack config): Add image support
    Can now import images directly into js files

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,10 @@ let config = {
           }
         },
         {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
           type: 'asset/resource'
           },
