0b6852462b9a65c60a95a040d39ab6fecce3955e
Brian Thomas <btdeveloper125@gmail.com>
2023-02-10 13:05:03 -0700

    feat(Crew page): Setup base page styles/HTML

--- app/assets/scripts/modules/CrewContent.js
+++ app/assets/scripts/modules/CrewContent.js
@@ -6,15 +6,15 @@ function CrewContent() {
     return(
       <div className='page__crew--content' key={item.name}>
         <div className='page__crew--main'>
-          <h1>{item.role.toLocaleUpperCase()}</h1>
-          <h1>{item.name}</h1>
-          <p>{item.bio}</p>
+          <h1 className='page__crew--text-role'>{item.role.toLocaleUpperCase()}</h1>
+          <h1 className='page__crew--text-name'>{item.name.toLocaleUpperCase()}</h1>
+          <p className='page__crew--text-bio'>{item.bio}</p>
           <div className='page__crew--menu-container'>
             <ul className="page__crew--menu">
               {
                 data.crew.map((index, i) => {
                   return(
-                    <li crew-index={i+1} className='page__crew--pagination-number' key={Math.floor((Math.random() * 100) + 1)}>
+                    <li crew-index={i+1} className='page__crew--pagination-number' key={i}>
                       <span className='page__crew--marker'></span>
                     </li>
                   )
@@ -24,7 +24,7 @@ function CrewContent() {
           </div>
         </div>
         
-        <img className='page__crew--image' src={`assets/images/crew/image-${name[0].toLocaleLowerCase()}-${name[1].toLocaleLowerCase()}.webp`} alt={`Image of crew member ${item.name}`} />
+        <img className={`page__crew--image`} id={name[1].toLocaleLowerCase()} src={`assets/images/crew/image-${name[0].toLocaleLowerCase()}-${name[1].toLocaleLowerCase()}.webp`} alt={`Image of crew member ${item.name}`} />
       </div>
     )
   })
