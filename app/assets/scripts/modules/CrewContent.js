b00b1d2bc34262b18c0939b3fb87773ad5b0c734
Brian Thomas <btdeveloper125@gmail.com>
2023-02-14 06:51:15 -0700

    refactor(Image SVG): Added the line svg for the bottom of image

--- app/assets/scripts/modules/CrewContent.js
+++ app/assets/scripts/modules/CrewContent.js
@@ -23,7 +23,9 @@ function CrewContent() {
             </ul>
           </div>
         </div>
-        
+        <svg version="1.1" className="page__crew--line"  x="0px" y="0px"  >
+          <path className="path2"  strokeWidth="2.3" d="M0 0 l1120 0" />
+        </svg>
         <img className={`page__crew--image`} id={name[1].toLocaleLowerCase()} src={`assets/images/crew/image-${name[0].toLocaleLowerCase()}-${name[1].toLocaleLowerCase()}.webp`} alt={`Image of crew member ${item.name}`} />
       </div>
     )
