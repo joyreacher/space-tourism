9ab9f767aab6e4b355441616fba390577316896e
Brian Thomas <btdeveloper125@gmail.com>
2023-02-16 08:59:15 -0700

    refactor(Technology page): Setup technology page
    Added more text from data.json file
    programm

--- app/assets/scripts/modules/TechContent.js
+++ app/assets/scripts/modules/TechContent.js
@@ -2,10 +2,9 @@ import React from 'react'
 import data from '../../data/data.json'
 function TechContent() {
   return data.technology.map((item) => {
-    console.log(item)
     // let name = item.name.split(' ')
     return(
-      <div className='page__technology--content' >
+      <div className='page__technology--content' key={item.name}>
         <div className='page__technology--main'>
         
           <div className='page__technology--menu-container'>
@@ -13,22 +12,23 @@ function TechContent() {
               {
                 data.technology.map((index, i) => {
                   return(
-                    <li technology-index={i+1} className='page__technology--pagination-number' key={Math.floor((Math.random() * 100) + 1)}>
-                      <span className='page__technology--marker'></span>
+                    <li technology-index={i+1} className='page__technology--pagination-number' key={i+1}>
+                      <span className='page__technology--marker'>{i+1}</span>
                     </li>
                   )
                 })
               }
             </ul>
           </div>
-          <div>
-            <h1>{item.name}</h1>
-            <p>{item.description}</p>
+          <div className="page__technology--copy-container">
+            <p className="page__technology--copy">THE TERMINOLOGY...</p>
+            <h1 className="page__technology--name">{item.name.toLocaleUpperCase()}</h1>
+            <p className="page__technology--description">{item.description}</p>
           </div>
           
         </div>
         
-        <img className='page__technology--image' src={item.images.landscape} alt={`Image of crew member ${item.name}`} />
+        <img className='page__technology--image' src={item.images.portrait} alt={`Image of crew member ${item.name}`} />
       </div>
     )
   })
